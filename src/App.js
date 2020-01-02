4d397ee9af943c7d7ff71bce5869055d62618718
“mishealcodile” <ezekielmisheal4@gmail.com>
2020-01-02 22:22:59 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -5,49 +5,93 @@ import Person from './component/Person'
 class App extends Component {
   state={
     persons:[
-      {name:"james", age:30},
-      { name: "misheal", age:40 },
-      { name: "cater", age:24 },
-      {name:"ifeoluwa", age:45},
+      {id:1,name:"james", age:30},
+      {id:2, name: "misheal", age:40 },
+      {id:3, name: "cater", age:24 },
+      {id:4,name:"ifeoluwa", age:45},
 
-    ]
-  }
-  changeName =() =>{
-    this.setState({
-      persons: [
-        { name: "moses", age: 30 },
-        { name: "tob", age: 50 },
-        { name: "cater", age: 24 },
-        { name: "maxwell", age: 45 },
-
-      ]
-    })
+    ],
+    showPersons : false
   }
 
-  nameChange =(event) =>{
-    this.setState({
-      persons: [
-        { name: "moses", age: 30 },
-        { name: "cater", age: 50 },
-        { name: event.target.value, age: 24 },
-        { name: "maxwell", age: 45 },
 
-      ]
+  nameChange =(event, id) =>{
+    const personIndex = this.state.persons.findIndex(name =>{
+     return name.id === id
     })
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name=event.target.value
+    const persons =[...this.state.persons]
+    persons[personIndex] = person
+
+    this.setState({persons:persons})
+  }
+  
+  deleteNameHandler =(personIndex)=>{
+    const persons =this.state.persons.slice()
+    persons.splice(personIndex, 1)
+    this.setState({persons:persons})
+  }
+
+  toggleNameHandler = () =>{
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow})
   }
 
   render() {
-    return (
-      <div className="App">
-        <h1>this is the main page</h1>
-        <button className="mainButton" onClick={this.changeName}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} nameChanged={this.nameChange}/>
-        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
+    const style ={
+      border: '1px solid blue',
+      color: 'white',
+      background: '#333',
+      font: 'inherit',
+      padding:'1.2rem',
+      
 
-      </div>
-    )
+    }
+
+    let persons = null
+    if(this.state.showPersons){
+      persons = (
+
+        <div>
+          {this.state.persons.map((person, index) =>{
+            return <Person  
+                  click={()=>this.deleteNameHandler(index)}
+                  name={person.name} 
+                  age={person.age}
+                  key={person.id}
+                  changed={(event)=>this.nameChange(event, person.id)}
+              
+              />
+          })}
+
+        </div>
+      )
+    }
+
+  const classes = []
+  if(this.state.persons.length <=2){
+    classes.push('red')
+  }
+  if(this.state.persons.length <=1){
+    classes.push('bold')
+  }
+    
+    return (
+      
+        <div className="App">
+          <h1 className={classes.join(" ")}>this is the main page</h1>
+          <button style={style} onClick={this.toggleNameHandler}>
+            Toggle Name
+          </button>
+          {persons}
+        </div>
+      
+    );
   }
 }
 
--- src/component/Person.js
+++ src/component/Person.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
 import './Person.css'
 
-export default class Person extends Component {
+class Person extends Component {
     render() {
+        // adding media queries using radium
+        // const style = {
+        //     '@media(min-width:500px)':{
+        //         width:'450px'
+        //     }
+        // }
         return (
-            <div className="Person">
-                <h3>my name is {this.props.name} and am {this.props.age} years old</h3>
-                <input type="text" onChange={this.props.nameChanged}/>
+            <div className="Person" style={style}>
+                <h3 onClick={this.props.click}>my name is {this.props.name} and am {this.props.age} years old</h3>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
             </div>
         )
     }
 }
 
 
- 
+export default Person
